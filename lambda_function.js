d2aa94adc872430d4e92f3444b2b16b3d72667ae
Meerab_shafique <meerab.shafique.ext@aquasec.com>
2023-02-14 13:50:18 +0500

    updated lambda

---
+++ lambda_function.js
@@ -0,0 +1,45 @@
+
+
+exports.handler = async (event) => {
+    const     intent = event['sessionState']['intent']['name']
+    const sessionAttributes = event['sessionState']['sessionAttributes']
+    if (!sessionAttributes) {
+      sessionAttributes = {};
+    }
+  
+    console.log(`Lex event info = ${JSON.stringify(event)}`);
+    console.log(`lambda_handler: session_attributes = ${JSON.stringify(sessionAttributes)}`);
+  
+  
+    if (HANDLERS[intent]) {
+      return HANDLERS[intent].handler(event, sessionAttributes);
+    }
+  };
+  
+  function checkAccountBalanceHandler(intentRequest, sessionAttributes) {
+    const { inputTranscript } = intentRequest;
+    const accountId = inputTranscript;
+    
+    return {
+      sessionState: {
+        dialogAction: {
+          type: 'Close',
+        },
+        intent: {
+          name: intentRequest.sessionState.intent.name,
+          state: 'Fulfilled',
+        },
+      },
+      messages: [
+        {
+          contentType: 'PlainText',
+          content: `Your current balance for account id ${accountId} is PKR/-10,000`,
+        },
+      ],
+    };
+  }
+  
+  const HANDLERS = {
+    checkAccountBalance: { handler: checkAccountBalanceHandler },
+  };
+  
