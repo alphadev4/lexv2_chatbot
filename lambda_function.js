95c2ec993cca4cc32787d0c23aa608d436754d58
Meerab_shafique <meerab.shafique.ext@aquasec.com>
2023-02-16 12:49:00 +0500

    empty

--- lambda_function.js
+++ lambda_function.js
@@ -1,25 +1,24 @@
 
 
 exports.handler = async (event) => {
-    const     intent = event['sessionState']['intent']['name']
-    const sessionAttributes = event['sessionState']['sessionAttributes']
-    if (!sessionAttributes) {
-      sessionAttributes = {};
-    }
-  
-    console.log(`Lex event info = ${JSON.stringify(event)}`);
-    console.log(`lambda_handler: session_attributes = ${JSON.stringify(sessionAttributes)}`);
-  
-  
-    if (HANDLERS[intent]) {
-      return HANDLERS[intent].handler(event, sessionAttributes);
-    }
-  };
-  
-  function checkAccountBalanceHandler(intentRequest, sessionAttributes) {
-    const { inputTranscript } = intentRequest;
-    const accountId = inputTranscript;
-    
+  const intent = event['sessionState']['intent']['name']
+  const sessionAttributes = event['sessionState']['sessionAttributes']
+  if (!sessionAttributes) {
+    sessionAttributes = {};
+  }
+
+  console.log(`Lex event info = ${JSON.stringify(event)}`);
+  console.log(`lambda_handler: session_attributes = ${JSON.stringify(sessionAttributes)}`);
+
+
+  if (HANDLERS[intent]) {
+    return HANDLERS[intent].handler(event, sessionAttributes);
+  }
+};
+
+function checkAccountBalanceHandler(intentRequest, sessionAttributes) {
+  const { inputTranscript } = intentRequest;
+  const accountId = inputTranscript;
     return {
       sessionState: {
         dialogAction: {
@@ -37,9 +36,28 @@ exports.handler = async (event) => {
         },
       ],
     };
-  }
-  
-  const HANDLERS = {
-    checkAccountBalance: { handler: checkAccountBalanceHandler },
-  };
-  
+}
+
+function checkOkResponse(intentRequest, sessionAttributes){
+    return {
+      sessionState: {
+        dialogAction: {
+          type: 'Close',
+        },
+        intent: {
+          name: intentRequest.sessionState.intent.name,
+          state: 'Fulfilled',
+        },
+      },
+      messages: [
+        {
+          contentType: 'PlainText',
+          content: `Great 😊`,
+        },
+      ],
+    };
+}
+const HANDLERS = {
+  checkAccountBalance: { handler: checkAccountBalanceHandler },
+  OkResponse: { handler: checkOkResponse}
+};
